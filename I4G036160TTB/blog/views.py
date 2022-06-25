fceb10e37f99a331414c43505a9eb5b521505612
Mosope-Codes <moshopeowo@gmail.com>
2022-06-25 07:37:00 +0100

    'added deleteView'

--- I4G036160TTB/blog/views.py
+++ I4G036160TTB/blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from django.urls import reverse, reverse_lazy
 
 from .models import Post
@@ -21,6 +22,11 @@ class PostDetailView(DetailView):
     model = Post
 
 class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+
+class PostDeleteView(DeleteView):
     model = Post
     fields = '__all__'
     success_url  = reverse_lazy('blog:all')
